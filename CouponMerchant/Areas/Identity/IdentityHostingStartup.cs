8d87bcd7005b6ad2266faad89795a95596a231c5
Pierre <pgadea@users.noreply.github.com>
2019-04-18 20:49:47 -0500

    Add project files.

---
+++ CouponMerchant/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CouponMerchant.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CouponMerchant.Areas.Identity.IdentityHostingStartup))]
+namespace CouponMerchant.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
